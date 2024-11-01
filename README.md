669332f6ed2b611d3744e1086d453603a8a41339
Niloy Ahsan <niloyahsan786@gmail.com>
2024-11-02 04:02:42 +0600

    updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# word-count
+# Word Count
+---
+- Count Word from here: [Link](https://niloyahsan1.github.io/word-count/)
--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const wordCountDisplay = document.getElementById("wordCount");
 const charCountDisplay = document.getElementById("charCount");
 
 textInput.addEventListener("input", () => {
-    const text = textInput.value.trim();
+    const texts = textInput.value.trim();
 
-    const words = text.split(/\s+/).filter((word) => word.length > 0);
+    const words = texts.split(/\s+/).filter((word) => word.length > 0);
 
     wordCountDisplay.textContent = words.length;
-    charCountDisplay.textContent = text.length;
+    charCountDisplay.textContent = texts.length;
 });
