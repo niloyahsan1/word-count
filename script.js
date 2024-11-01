31cda85d04bdfa1a1890078eeb80351064b93e79
Niloy Ahsan <157811017+niloyahsan1@users.noreply.github.com>
2024-11-02 04:13:56 +0600

    updated

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const wordCountDisplay = document.getElementById("wordCount");
 const charCountDisplay = document.getElementById("charCount");
 
 textInput.addEventListener("input", () => {
-    const texts = textInput.value.trim();
+    const text = textInput.value.trim();
 
-    const words = texts.split(/\s+/).filter((word) => word.length > 0);
+    const words = text.split(/\s+/).filter((word) => word.length > 0);
 
     wordCountDisplay.textContent = words.length;
-    charCountDisplay.textContent = texts.length;
+    charCountDisplay.textContent = text.length;
 });
